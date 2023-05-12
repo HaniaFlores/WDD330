2a7b7e7eba032d6a122d15c35dd74ea172af53ef
dcarsin <dcarsin@byui.edu>
2023-05-11 23:16:22 -0300

    commit 2nd time

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,3 @@
-import  productList  from "./productList.mjs"
+import productList from "./productList.mjs";
 
-let products = productList(".product-list", "tents")
+let products = productList(".product-list", "tents");
