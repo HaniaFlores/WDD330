70a90213a2fe4bcc81aeed3ca7f203c450de6f41
HaniaFlores <hanniaflores03@gmail.com>
2023-05-29 11:16:07 -0600

    newsletter js and html

--- src/js/main.js
+++ src/js/main.js
@@ -11,3 +11,16 @@ loadHeaderFooter();
 document
   .querySelector("#sort-by-price")
   .addEventListener("click", () => sortBy("sort-by-price")); */
+
+function newsletterSubscription(event) {
+  event.preventDefault();
+
+  const emailEl = document.getElementById("email-input");
+  const email = emailEl.value;
+  alert("Thank you for subscribing to our newsletter!\n" + email);
+  emailEl.value = "";
+  // Not sure about what to do with the email address. Decided to return it as a string.
+  return email;
+}
+
+document.getElementById("newsletter-box").addEventListener("submit", newsletterSubscription);
