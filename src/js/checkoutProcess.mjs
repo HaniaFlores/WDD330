36dfd71c382cd701a50637a798b3465cde2ce80a
HaniaFlores <hanniaflores03@gmail.com>
2023-06-02 12:31:43 -0600

    form submition

--- src/js/checkoutProcess.mjs
+++ src/js/checkoutProcess.mjs
@@ -1,4 +1,32 @@
 import { getLocalStorage } from "./utils.mjs";
+import { checkout } from "./externalServices.mjs";
+
+// takes a form element and returns an object where the key is the "name" of the form input.
+function formDataToJSON(formElement) {
+  const formData = new FormData(formElement),
+    convertedJSON = {};
+
+  formData.forEach(function (value, key) {
+    convertedJSON[key] = value;
+  });
+
+  return convertedJSON;
+}
+
+// takes the items currently stored in the cart (localstorage) and returns them in a simplified form.
+function packageItems(items) {
+// convert the list of products from localStorage to the simpler form required for the checkout process. Array.map would be perfect for this.
+  const simplifiedItems = items.map((item) => {
+    console.log("package items", item);
+    return {
+      id: item.Id,
+      price: item.FinalPrice,
+      name: item.Name,
+      quantity: 1,
+    };
+  });
+  return simplifiedItems;
+}
 
 const checkoutProcess = {
     key: "",
@@ -50,7 +78,25 @@ const checkoutProcess = {
     shipping.innerText = "$" + this.shipping;
     tax.innerText = "$" + this.tax;
     orderTotal.innerText = "$" + this.orderTotal;
-  }
+  },
+  checkout: async function (form) {
+    const json = formDataToJSON(form);
+    // build the data object from the calculated fields, the items in the cart, and the information entered into the form
+    // add totals, and item details
+    json.orderDate = new Date();
+    json.orderTotal = this.orderTotal;
+    json.tax = this.tax;
+    json.shipping = this.shipping;
+    json.items = packageItems(this.list);
+    console.log("json", json);
+    // call the checkout method in our externalServices module and send it our data object.
+    try {
+      const res = await checkout(json);
+      console.log(res);
+    } catch (err) {
+      console.log(err);
+    }
+  },
   
 }
 export default checkoutProcess;
