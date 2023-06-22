5a5c9d69e90cd7a30b451594264877b2f346209f
HaniaFlores <hanniaflores03@gmail.com>
2023-06-21 18:18:50 -0600

    product__color

--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -9,28 +9,6 @@ import {
 let product = {};
 const discountRate = 0.1;
 
-/* function addToCart() {
-  let cartContents = getLocalStorage("so-cart");
-  product.Quantity = 1;
-  //check to see if there was anything there
-  if (!cartContents) {
-    cartContents = [];
-  }
-
-  const duplicateProduct = cartContents.find(item => item.Id === product.Id);
-  if (duplicateProduct) {
-    product.Quantity += 1;
-  } else {
-    cartContents.push(product);
-  }
-  // then add the current product to the list
-  // cartContents.push(product);
-  console.log(product);
-  setLocalStorage("so-cart", cartContents);
-  animationIcon();
-  alertMessage("Item added succesfully!");
-} */
-
 function addToCart() {
   let cartContents = getLocalStorage("so-cart");
   product.Quantity = 1;
@@ -84,6 +62,8 @@ function renderProductDetails() {
   document.getElementById("productDescriptionHtmlSimple").innerHTML =
     product.DescriptionHtmlSimple;
   document.getElementById("addToCart").setAttribute("data-id", product.Id);
+  const productColor = document.querySelector(".product__color").innerText;
+  document.getElementById("addToCart").setAttribute("color-code", productColor);
 }
 
 export default async function productDetails(productId) {
@@ -96,6 +76,7 @@ export default async function productDetails(productId) {
   //GR--html-error-empty-cart-main
   renderProductDetails();
   setColorItems();
+  console.log("json", product)
   document.getElementById("addToCart").addEventListener("click", addToCart);
 }
 
@@ -117,6 +98,8 @@ function setColorItems() {
       newImage.setAttribute("alt", alt);
       newImage.setAttribute("src", src);
       document.querySelector(".product__color").innerText = alt;
+      const productColor = document.querySelector(".product__color").innerText;
+      document.getElementById("addToCart").setAttribute("color-code", productColor);
     });
   });
 }
